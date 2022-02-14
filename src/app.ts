4ee379fe80917b0da3be8944ea86b48a34f2ebf0
Alex-dev02 <ionut02alex@yahoo.ro>
2022-02-14 20:04:06 +0200

    socket.io init

--- src/app.ts
+++ src/app.ts
@@ -2,13 +2,17 @@ import Koa from 'koa';
 import "reflect-metadata";
 import cors from '@koa/cors'
 import logger from 'koa-logger';
+import { Server } from 'http';
 
 import { getRouters } from './utils/router';
 import { createConn } from './utils/database';
 
 const app = new Koa();
-app.use(logger());
+const io = new Server();
 
+app.use(cors({origin: '*'}));
+
+app.use(logger());
 
 createConn();
 
@@ -17,5 +21,9 @@ const routers = getRouters();
 routers.forEach(router => {
     app.use(router.routes());
 });
+
+io.on('connection', (socket) => {
+    console.log('connected');
+})
 
 app.listen(process.env.PORT || 4000);

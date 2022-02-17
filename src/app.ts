722b65faf81febd78596b9959030c68402f370b3
Alex-dev02 <ionut02alex@yahoo.ro>
2022-02-17 11:02:02 +0200

    emit socket event on new tvalue

--- src/app.ts
+++ src/app.ts
@@ -2,16 +2,22 @@ import Koa from 'koa';
 import "reflect-metadata";
 import cors from '@koa/cors'
 import logger from 'koa-logger';
-import { Server } from 'http';
+
+import { createServer } from 'http';
+import { Server } from 'socket.io';
+
 
 import { getRouters } from './utils/router';
 import { createConn } from './utils/database';
 
 const app = new Koa();
-const io = new Server();
-
+const httpServer = createServer(app.callback());
+const io = new Server(httpServer, {});
 app.use(cors({origin: '*'}));
-
+app.use(async (ctx: any, next: any) => {
+  ctx.state.io = io;
+  await next();
+});
 app.use(logger());
 
 createConn();
@@ -19,11 +25,7 @@ createConn();
 const routers = getRouters();
 
 routers.forEach(router => {
-    app.use(router.routes());
+  app.use(router.routes());
 });
 
-io.on('connection', (socket) => {
-    console.log('connected');
-})
-
-app.listen(process.env.PORT || 4000);
+httpServer.listen(4000);
--- src/controllers/tvalue.controller.ts
+++ src/controllers/tvalue.controller.ts
@@ -7,33 +7,40 @@ const router = new Router();
 router.prefix('/tvalues');
 
 router.post('/', async (ctx: any, next: any): Promise<TValue> => {
-    return ctx.body = await saveTValue(ctx.query.value, ctx.query.boardId);
+  const tvalue: TValue =  await saveTValue(ctx.query.value, ctx.query.boardId);
+  const io = ctx.state.io;
+  io.emit("new-tvalue", tvalue);
+  return ctx.body = tvalue;
+});
+
+router.get('/ping', async (ctx: any, next: any): Promise<string> => {
+  return ctx.body = 'OK';
 });
 
 router.get('/single', async (ctx: any, next:any): Promise<TValue | undefined> => {
-    return ctx.body = await getTValueById(ctx.query.id);
+  return ctx.body = await getTValueById(ctx.query.id);
 });
 
 router.get('/last', async (ctx: any, next: any): Promise<TValue | undefined> => {
-    return ctx.body = await getLastAddedValue();
+  return ctx.body = await getLastAddedValue();
 });
 
 router.get('/', async (ctx: any, next: any): Promise<TValue[]> => {
-    return ctx.body = await getAllTValues();
+  return ctx.body = await getAllTValues();
 });
 
 router.put('/', async(ctx: any, next: any): Promise<TValue> => {
-    return ctx.body = await updateTValue({
-        id: ctx.query.id,
-        value: ctx.query.value,
-        boardId: ctx.query.boardId,
-        created: ctx.query.created,
-        updated: ctx.query.updated,
-    });
+  return ctx.body = await updateTValue({
+    id: ctx.query.id,
+    value: ctx.query.value,
+    boardId: ctx.query.boardId,
+    created: ctx.query.created,
+    updated: ctx.query.updated,
+  });
 });
 
 router.delete('/', async(ctx: any, next: any): Promise<DeleteResult> => {
-    return ctx.body = await deleteTValue(ctx.query.id);
-})
+  return ctx.body = await deleteTValue(ctx.query.id);
+});
 
 export default router;

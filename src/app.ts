9a6b4e46c7486e85253fd80f0d3fd1a2dd1a7e80
Alex-dev02 <ionut02alex@yahoo.ro>
2022-03-02 11:09:31 +0200

    log requests

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import Koa from 'koa';
 import "reflect-metadata";
 import cors from '@koa/cors'
 import logger from 'koa-logger';
+import koaBody from 'koa-body';
+import fileStream from 'fs';
 
 import { createServer } from 'http';
 import { Server } from 'socket.io';
@@ -10,15 +12,28 @@ import { getRouters } from './utils/router';
 import { createDBConnection } from './utils/database';
 
 const app = new Koa();
+app.use(koaBody());
+
 const httpServer = createServer(app.callback());
 const io = new Server(httpServer, {});
+
 app.use(cors({
   origin: process.env.ENV === 'DEV' ? '*' : process.env.ORIGIN 
 }));
+
+app.use((ctx: any, next: any) => {
+  fileStream.appendFileSync(
+    'logs.txt', `${new Date()} ` + JSON.stringify(ctx) +
+      '\nreq.body:' + JSON.stringify(ctx.request.body) + '\n'
+    );
+  next();
+})
+
 app.use(async (ctx: any, next: any) => {
   ctx.state.io =  io;
   await next();
 });
+
 app.use(logger());
 
 createDBConnection();
